152666319750c6894cd1c36cbd6ebb001b73a2ef
Brad Montgomery <brad@bradmontgomery.net>
2012-03-29 15:23:37 -0500

    version bump

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "stephen@atalanta-systems.com"
 license          "Apache 2.0"
 description      "Installs/Configures supervisord"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "0.1.1"
 
 recipe           "supervisord::default", "Installs/Configures supervisord" 
 recipe           "supervisord::includes", "Installs/Configures additional programs" 
